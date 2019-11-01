a4a647005090c05627e19f0f6694a382f150f5ba
kaiscript <teardownk@gmail.com>
2019-11-01 08:45:08 +0800

    RotateArray

---
+++ src/Algorithms/Array/RotateArray.java
@@ -0,0 +1,57 @@
+package Algorithms.Array;
+
+/**
+ * Rotate Array
+ * Solution
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * Note:
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ */
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        //余长度，求出真正需要移动的步数
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        RotateArray ra = new RotateArray();
+        int[] nums = new int[]{1, 2};
+        ra.rotate(nums, 3);
+        for (int num : nums) {
+            System.out.print(num+" ");
+        }
+    }
+
+}
