806f1dd2030e97bf6a6cdeefe267680a2b4019c9
kaiscript <teardownk@gmail.com>
2019-11-19 23:21:37 +0800

    bfs

--- src/Algorithms/leetcode/queue/NumberOfIslands.java
+++ src/Algorithms/leetcode/queue/NumberOfIslands.java
@@ -3,6 +3,7 @@ package Algorithms.leetcode.queue;
 import java.util.Stack;
 
 /**
+ * 200.
  * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
  * You may assume all four edges of the grid are all surrounded by water.
@@ -31,21 +32,59 @@ public class NumberOfIslands {
     public static void main(String[] args) {
         NumberOfIslands oi = new NumberOfIslands();
         char[][] grid = new char[4][5];
-        grid[0][0] = 1;
-        grid[0][1] = 1;
-        grid[0][2] = 1;
-        grid[0][3] = 1;
-        grid[0][4] = 0;
+        grid[0][0] = '1';
+        grid[0][1] = '1';
+        grid[0][2] = '1';
+        grid[0][3] = '1';
+        grid[0][4] = '0';
 
-        grid[1][0] = 1;
-        grid[1][1] = 1;
-        grid[1][3] = 1;
+        grid[1][0] = '1';
+        grid[1][1] = '1';
+        grid[1][3] = '1';
 
-        grid[2][0] = 1;
-        grid[2][1] = 1;
-        System.out.println(oi.numIslands(grid));
+        grid[2][0] = '1';
+        grid[2][1] = '1';
+        System.out.println(oi.numIslandsDfs(grid));
     }
 
+    public int numIslandsDfs(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * dfs寻找，并将找到的陆地置零，直到没陆地为止
+     * @param grid
+     * @param x
+     * @param y
+     */
+    private void dfs(char[][] grid, int x, int y) {
+        if (x >= grid.length || x < 0 || y >= grid[x].length || y < 0 || grid[x][y] == '0') {
+            return;
+        }
+        grid[x][y] = '0';
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+    }
+
+    /**
+     * bfs
+     * @param grid
+     * @return
+     */
     public int numIslands(char[][] grid) {
         if (grid.length == 0) {
             return 0;
