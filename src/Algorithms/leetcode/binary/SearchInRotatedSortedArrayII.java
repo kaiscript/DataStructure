dcd85416745f04e534991639eaeefb22d6c5c970
chenkai1 <chenkai1@yy.com>
2019-11-22 17:42:31 +0800

    binary

--- src/Algorithms/leetcode/binary/SearchInRotatedSortedArrayII.java
+++ src/Algorithms/leetcode/binary/SearchInRotatedSortedArrayII.java
@@ -62,31 +62,4 @@ public class SearchInRotatedSortedArrayII {
         System.out.println(searchInRotatedSortedArrayII.search(nums, 3));
     }
 
-    /**
-     *
-     *         mid = (start + end) / 2;
-     *         if (nums[mid] == target) {
-     *             return true;
-     *         }
-     *         //If we know for sure right side is sorted or left side is unsorted
-     *         if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
-     *             if (target > nums[mid] && target <= nums[end]) {
-     *                 start = mid + 1;
-     *             } else {
-     *                 end = mid - 1;
-     *             }
-     *             //If we know for sure left side is sorted or right side is unsorted
-     *         } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
-     *             if (target < nums[mid] && target >= nums[start]) {
-     *                 end = mid - 1;
-     *             } else {
-     *                 start = mid + 1;
-     *             }
-     *             //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
-     *             //any of the two sides won't change the result but can help remove duplicate from
-     *             //consideration, here we just use end-- but left++ works too
-     *         } else {
-     *             end--;
-     *         }
-     */
 }
