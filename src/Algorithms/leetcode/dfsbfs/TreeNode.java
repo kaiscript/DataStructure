97b6e4cad82173ba95e788db6f210bbaf244cc5e
chenkai1 <chenkai1@yy.com>
2019-12-12 12:45:40 +0800

    bfs:BinaryTreeLevelOrderTraversal

---
+++ src/Algorithms/leetcode/dfsbfs/TreeNode.java
@@ -0,0 +1,17 @@
+package Algorithms.leetcode.dfsbfs;
+
+/**
+ * @author chenkai
+ * @date: 2019-12-12
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
