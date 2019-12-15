6948eff172f4545d5ba0f0df57d511e7a86b3814
kaiscript <teardownk@gmail.com>
2019-12-15 11:58:08 +0800

    binary tree

--- src/Algorithms/leetcode/stringAndArray/AddBinary.java
+++ src/Algorithms/leetcode/stringAndArray/AddBinary.java
@@ -1,6 +1,7 @@
 package Algorithms.leetcode.stringAndArray;
 
 /**
+ * 67.
  * Given two binary strings, return their sum (also a binary string).
 
  The input strings are both non-empty and contains only characters 1 or 0.
--- src/Algorithms/leetcode/stringAndArray/MoveZeroes.java
+++ src/Algorithms/leetcode/stringAndArray/MoveZeroes.java
@@ -1,6 +1,7 @@
 package Algorithms.leetcode.stringAndArray;
 
 /**
+ * 283.
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  *
  * Example:
